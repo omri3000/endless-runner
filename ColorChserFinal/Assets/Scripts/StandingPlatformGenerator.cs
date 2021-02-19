3016e4b354a76d4aaa6d80e25f1e30ba20428d77
ben <ben@gamejamtech.com>
2021-02-19 19:08:40 +0200

    added types of platforms and diffrent length mech

--- ColorChserFinal/Assets/Scripts/StandingPlatformGenerator.cs
+++ ColorChserFinal/Assets/Scripts/StandingPlatformGenerator.cs
@@ -4,25 +4,43 @@ using UnityEngine;
 
 public class StandingPlatformGenerator : MonoBehaviour
 {
-    public GameObject platform;
+    ///public GameObject greenPlatform;
+    //public GameObject redPlatform;
+    //public GameObject blackPlatform;
     public Transform generatingPoint;
     public float distanceBetween;
-    public float platformWidth;
 
     public float distanceBetweenMax;
     public float distanceBetweenMin;
 
+    public float minHeight;
+    public float maxHeight;
+    public float maxHeightChange;
+    public float minHeightChange;
+    private float hightChange;
+    public float platformMinLength;
+    public float platformMaxLength;
+    private float numOfBlocksCreated;
+    public GameObject[] platformTypes;
+
     void Start()
     {
-        platformWidth = platform.GetComponent<BoxCollider2D>().size.x;
+        minHeight = transform.position.y;
+        hightChange = minHeight + Random.Range(0,maxHeightChange);
+        numOfBlocksCreated = 0;
     }
 
     void Update()
     {
         if (transform.position.x < generatingPoint.position.x){
             distanceBetween = Random.Range(distanceBetweenMin,distanceBetweenMax);
-            transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween,transform.position.y,transform.position.z);
-            Instantiate(platform,transform.position,Quaternion.Euler(0, 0, 90));
+            transform.position = new Vector3(transform.position.x + distanceBetween,hightChange,transform.position.z);
+            float platformLength = Random.Range(platformMinLength,platformMaxLength);
+            GameObject typeofBlockToInstance = platformTypes[Random.Range(0,platformTypes.Length)];
+            for(int i = 0; i < platformLength; i++)
+        {
+            Instantiate(typeofBlockToInstance,new Vector3(transform.position.x,transform.position.y + i,transform.position.z),transform.rotation);
+        }
         }
     }
 
