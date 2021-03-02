7920cbc134bd95b1c60439623e839158fc07ae0b
Omri Cohen <omri3000@gmail.com>
2021-03-02 18:59:38 +0200

    add sound manager bg Parallax tail

---
+++ ColorChserFinal/Assets/SoundManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+
+
+    public AudioSource sound;
+    public AudioClip jumpSound;
+    public AudioClip bgSound;
+
+    private void Awake()
+    {
+        if (instance != this && instance != null)
+        {
+            Destroy(gameObject);
+        }
+        instance = this;
+    }
+
+    public void playBGSound()
+    {
+        sound.clip = bgSound;
+        sound.Play();
+    }
+
+    public void playJumpSound()
+    {
+        sound.PlayOneShot(jumpSound);
+    }
+}
